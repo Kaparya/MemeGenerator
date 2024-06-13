879c8d84acfe90a558c32d0dc07c0cdc7b0526d9
Damir Kandelov <kaparyaka@mail.ru>
2024-06-14 00:30:09 +0300

    Refactor code structure

--- src/workWithImages/addText.py
+++ src/workWithImages/addText.py
@@ -16,46 +16,49 @@ def checkBounds(leftPoint, rightPoint, bounds):
             raise Exception(f'Text plane: {leftPoint}, {rightPoint}\nOut of bounds: {bounds[0]} x {bounds[1]}')
 
 
+def addCurrentText(textNode, width, height, image):
+    leftPoint = [textNode['left_point']['x'], textNode['left_point']['y']]
+    rightPoint = [textNode['right_point']['x'], textNode['right_point']['y']]
+
+    text = textNode['text']
+    try:
+        checkBounds(leftPoint, rightPoint, (width, height))
+    except Exception as e:
+        print(f'\nCan\'t write text: \"{text}\"')
+        print(e)
+        return
+
+    draw = ImageDraw.Draw(image)
+
+    text = textNode['text']
+    imageSize = (rightPoint[0] - leftPoint[0], rightPoint[1] - leftPoint[1])
+
+    textSize = [0, 0]
+    textFont = ImageFont.load_default(1)
+    for i in range(1, 200):
+        textFont = ImageFont.load_default(i)
+        textSize = [0, 0]
+        _, _, textSize[0], textSize[1] = draw.multiline_textbbox((0, 0), text, font=textFont)
+
+        if textSize[0] > imageSize[0] * 0.95 or textSize[1] > imageSize[1] * 0.95:
+            break
+
+    centerPoint = [(rightPoint[0] + leftPoint[0]) / 2,
+                   (rightPoint[1] + leftPoint[1]) / 2]
+    centerPoint[0] -= textSize[0] / 2
+    centerPoint[1] -= textSize[1] / 2
+    print(centerPoint)
+
+    draw.text(centerPoint, text, fill=(0, 0, 0), font=textFont)
+
+
 def addText(imageData: json):
     print(imageData['file'])
     image = Image.open('images/' + imageData['file'] + '.png')
 
     width, height = image.size
     for textNode in imageData['texts']:
-        leftPoint = [textNode['left_point']['x'], textNode['left_point']['y']]
-        rightPoint = [textNode['right_point']['x'], textNode['right_point']['y']]
-
-        text = textNode['text']
-        try:
-            checkBounds(leftPoint, rightPoint, (width, height))
-        except Exception as e:
-            print(f'\nCan\'t write text: \"{text}\"')
-            print(e)
-            continue
-
-        draw = ImageDraw.Draw(image)
-
-        text = textNode['text']
-        imageSize = (rightPoint[0] - leftPoint[0], rightPoint[1] - leftPoint[1])
-
-        textSize = [0, 0]
-        textFont = ImageFont.load_default(1)
-        for i in range(1, 200):
-            textFont = ImageFont.load_default(i)
-            textSize = [0, 0]
-            _, _, textSize[0], textSize[1] = draw.multiline_textbbox((0, 0), text, font=textFont)
-
-            if textSize[0] > imageSize[0] * 0.95 or textSize[1] > imageSize[1] * 0.95:
-                break
-
-        centerPoint = [(rightPoint[0] + leftPoint[0]) / 2,
-                       (rightPoint[1] + leftPoint[1]) / 2]
-        centerPoint[0] -= textSize[0] / 2
-        centerPoint[1] -= textSize[1] / 2
-        print(centerPoint)
-
-        draw.text(centerPoint, text, fill=(0, 0, 0), font=textFont)
+        addCurrentText(textNode, width, height, image)
 
     image.save('results/' + imageData['file'] + '.png')
     # image.show()
-
