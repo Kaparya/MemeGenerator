18f6dd064d1c45763fe0b9cd81cf99875cc9fe12
Damir Kandelov <kaparyaka@mail.ru>
2024-06-14 23:34:07 +0300

    Add comments

--- src/workWithImages/addText.py
+++ src/workWithImages/addText.py
@@ -11,7 +11,7 @@ def checkBounds(leftPoint, rightPoint, bounds):
         if leftPoint[i] > rightPoint[i]:
             leftPoint[i], rightPoint[i] = rightPoint[i], leftPoint[i]
         elif leftPoint[i] == rightPoint[i]:
-            raise Exception(f'Text plane {leftPoint}, {rightPoint} is to small for the text\n')
+            raise Exception(f'Text plane {leftPoint}, {rightPoint} is too small for the text\n')
         if leftPoint[i] >= bounds[i] or leftPoint[i] < i \
                 or rightPoint[i] >= bounds[i] or rightPoint[i] < i:
             raise Exception(f'Text plane: {leftPoint}, {rightPoint} out of image bounds: {bounds[0]} x {bounds[1]}\n')
@@ -25,15 +25,15 @@ def addCurrentText(textNode, width, height, image):
     try:
         checkBounds(leftPoint, rightPoint, (width, height))
     except Exception as e:
-        print(f'\nCan\'t write text: \"{text}\"')
+        print(f'\nCan\'t add text: \"{text}\"')
         print(e)
         return
 
+    # Add text to image
     draw = ImageDraw.Draw(image)
-
     text = textNode['text']
+    # Calculate correct size of text by its rectangle size
     imageSize = (rightPoint[0] - leftPoint[0], rightPoint[1] - leftPoint[1])
-
     textSize = [0, 0]
     textFont = ImageFont.load_default(1)
     for i in range(1, 200):
@@ -44,6 +44,7 @@ def addCurrentText(textNode, width, height, image):
         if textSize[0] > imageSize[0] * 0.95 or textSize[1] > imageSize[1] * 0.95:
             break
 
+    # Place text in the center of rectangle
     centerPoint = [(rightPoint[0] + leftPoint[0]) / 2,
                    (rightPoint[1] + leftPoint[1]) / 2]
     centerPoint[0] -= textSize[0] / 2
@@ -53,6 +54,12 @@ def addCurrentText(textNode, width, height, image):
 
 
 def addTextToImage(imageData: json) -> str:
+    """
+    Adds text to image if it exists. Otherwise, it raises an exception FileNotFoundError.
+    :param imageData: JSON object of specified structure from server
+    :return: Name of new saved image with added text
+    """
+
     image = Image.open('images/' + imageData['file'] + '.png')
 
     width, height = image.size
