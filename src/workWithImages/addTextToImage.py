fa6ee85e15d46bfc5f4c7f21eca38f09a8f244ea
Damir Kandelov <kaparyaka@mail.ru>
2024-06-17 23:41:50 +0300

    Add server requests

--- src/workWithImages/addTextToImage.py
+++ src/workWithImages/addTextToImage.py
@@ -1,9 +1,10 @@
 import json
-from uuid import uuid4
 
+from pathlib import Path
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
+from uuid import uuid4
 
 
 def checkBounds(leftPoint, rightPoint, bounds):
@@ -11,22 +12,26 @@ def checkBounds(leftPoint, rightPoint, bounds):
         if leftPoint[i] > rightPoint[i]:
             leftPoint[i], rightPoint[i] = rightPoint[i], leftPoint[i]
         elif leftPoint[i] == rightPoint[i]:
-            raise Exception(f'Text plane {leftPoint}, {rightPoint} is too small for the text\n')
+            raise ValueError(f'Text plane {leftPoint}, {rightPoint} is too small for the text')
         if leftPoint[i] >= bounds[i] or leftPoint[i] < 0 \
                 or rightPoint[i] >= bounds[i] or rightPoint[i] < 0:
-            raise Exception(f'Text plane: {leftPoint}, {rightPoint} out of image bounds: {bounds[0]} x {bounds[1]}\n')
+            raise ValueError(f'Text plane: {leftPoint}, {rightPoint} out of image bounds: {bounds[0]} x {bounds[1]}')
 
 
-def addCurrentText(textNode, width, height, image):
+def addCurrentText(textNode, width, height, image, response: dict):
+    if 'left_point' not in textNode or 'right_point' not in textNode \
+            or 'text' not in textNode or 'x' not in textNode['left_point'] or 'y' not in textNode['left_point'] \
+            or 'x' not in textNode['right_point'] or 'y' not in textNode['right_point']:
+        raise KeyError('Wrong JSON structure')
+
     leftPoint = [textNode['left_point']['x'], textNode['left_point']['y']]
     rightPoint = [textNode['right_point']['x'], textNode['right_point']['y']]
 
     text = textNode['text']
     try:
         checkBounds(leftPoint, rightPoint, (width, height))
-    except Exception as e:
-        print(f'\nCan\'t add text: \"{text}\"')
-        print(e)
+    except ValueError as e:
+        response['errors'].append(f'Can\'t add text: \'{text}\' (' + str(e) + ')')
         return
 
     # Add text to image
@@ -53,19 +58,27 @@ def addCurrentText(textNode, width, height, image):
     draw.text(centerPoint, text, fill=(0, 0, 0), font=textFont)
 
 
-def addTextToImage(imageData: json) -> str:
+def addTextToImage(imageData: json, response: dict) -> str:
     """
     Adds text to image if it exists. Otherwise, it raises an exception FileNotFoundError.
     :param imageData: JSON object of specified structure from server
+    :param response: Response from server with errors
     :return: Name of new saved image with added text
     """
+    if 'file' not in imageData or 'texts' not in imageData:
+        raise KeyError('Wrong JSON structure')
 
-    image = Image.open('images/' + imageData['file'] + '.png')
+    try:
+        image = Image.open('images/' + imageData['file'] + '.png')
+    except FileNotFoundError:
+        raise FileNotFoundError(f'There is no image with name \'' + str(imageData['file']) + '\' on the server')
 
     width, height = image.size
     for textNode in imageData['texts']:
-        addCurrentText(textNode, width, height, image)
+        addCurrentText(textNode, width, height, image, response)
 
     newName = str(uuid4())
+    while Path('results/' + newName + '.png').exists():
+        newName = str(uuid4())
     image.save('results/' + newName + '.png')
     return newName
